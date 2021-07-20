150b6889119e8087aafb9f36289f3d5e38c2a8a7
matthung0807 <bronx0807@gmail.com>
2021-07-20 18:21:58 +0800

    React create component

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Hello, world!</h1>
+  const hello = <Hello name="John" />
+  return hello
+}
+
+// function Hello(props) {
+//   return <h1>Hello, {props.name}!</h1>
+// }
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}!</h1>
+  }
 }
 
 export default App;
