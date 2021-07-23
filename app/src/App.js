41931c400d5f2984c126591782369fb500fa6a07
matthung0807 <bronx0807@gmail.com>
2021-07-23 16:28:44 +0800

    React Router install and basic use

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,45 @@
 import './App.css';
 
-function App() {
-  return <h1>Hello, world!</h1>
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <Switch> 尋找其中的<Route>把第一個符合path的內容渲染到畫面 */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
