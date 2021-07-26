67d6a961b3a4ad43e42bebd5badbabda3fd9532b
matthung0807 <bronx0807@gmail.com>
2021-07-26 16:12:51 +0800

    React Error Boundaries catch components error

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,33 @@
-import loadable from '@loadable/component'
+import React from 'react'
 
 import './App.css';
 
-const Hello = loadable(() => import('./greeting/Hello'));
+const names = ["John", "Mary"]
 
-function App() {
-  return <Hello name = "John"/>
+const Hello = props => {
+  return <h1>Hello, {names[3].toLowerCase()}!</h1> // TypeError: Cannot read property 'toLowerCase' of undefined
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hasError: false}
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true }; // return a value to update state
+  }
+
+  componentDidCatch(error, info) {
+    console.log(`Cause:${error}\nStacktace:${info.componentStack}`)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Error!!</h1>
+    }
+
+    return <Hello/>
+  }
 }
 
 export default App;
