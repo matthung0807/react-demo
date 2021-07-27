d6f0b0ab8cbd81e1a70c482f37baded1030ba66e
matthung0807 <bronx0807@gmail.com>
2021-07-28 00:28:03 +0800

    React Render Props intro

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # react-demo
 React.js Demo
 
-[React 使用Create React App建立React應用程式](https://matthung0807.blogspot.com/2021/07/react-create-react-app.html)
+[React Render Props簡介](https://matthung0807.blogspot.com/2021/07/react-render-props-intro.html)
--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,69 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return <h1>Hello, world!</h1>
+  const HelloWithLike = withLike(Hello, {name: "John"});
+  const HiWithLike = withLike(Hi, {name: "Mary"});
+  return (
+    <div>
+      <HelloWithLike />
+      <HiWithLike />
+    </div>
+  );
+}
+
+function withLike(Component, props) {
+  return class extends React.Component {
+    render() {
+      return (
+        <Like>
+          {(handleLike, count) => (
+            <Component {...props} like={handleLike} count={count} />
+          )}
+        </Like>
+      );
+    }
+  };
+}
+
+class Like extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+
+    this.handleLike = this.handleLike.bind(this);
+  }
+
+  handleLike = () => {
+    let count = this.state.count;
+    this.setState({count: ++count});
+  };
+
+  render() {
+    return <div>{this.props.children(this.handleLike, this.state.count)}</div>;
+  }
+}
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 onClick={this.props.like}>Hello, {this.props.name}</h1>
+        <span>Like:{this.props.count}</span>
+      </div>
+    );
+  }
+}
+
+class Hi extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 onClick={this.props.like}>Hi, {this.props.name}</h1>
+        <span>Like:{this.props.count}</span>
+      </div>
+    );
+  }
 }
 
 export default App;
