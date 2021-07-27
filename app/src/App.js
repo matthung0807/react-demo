435239fb77f37dd932bd28739de5823ac8abb6ed
matthung0807 <bronx0807@gmail.com>
2021-07-27 09:16:28 +0800

    React Context pass data

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,50 @@
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+const CharacterContext = React.createContext(""); // default value is empty string
 
 function App() {
-  return <h1>Hello, world!</h1>
+  return (
+    <CharacterContext.Provider value='DIO'>
+      <Character />
+    </CharacterContext.Provider>
+  );
+}
+
+class Character extends React.Component {
+  static contextType = CharacterContext;
+  render() {
+    return (
+      <div>
+        <h1>{this.context}</h1>
+        <Intro />
+      </div>
+    );
+  }
+}
+
+class Intro extends React.Component {
+  static contextType = CharacterContext;
+  render() {
+    return (
+      <div>
+        <p>{this.context} is the main antagonist of JoJo's Bizarre Part 3.</p>
+        <Stand />
+      </div>
+    );
+  }
+}
+
+class Stand extends React.Component {
+  static contextType = CharacterContext;
+  render() {
+    return (
+      <div>
+        {this.context}'s stand is <strong>The World</strong>.
+      </div>
+    );
+  }
 }
 
 export default App;
